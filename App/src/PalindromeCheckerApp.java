5f403184afd0c4c7321a90055e6f421fb8fff7e5
pratistha09 <thepratisthachaira@gmail.com>
2026-02-26 18:32:51 +0530

    UC12: Strategy Pattern for Palindrome Algorithms (Advanced)

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,25 +1,21 @@
-class PalindromeChecker {
-    public boolean checkPalindrome(String input) {
-        if (input == null || input.isBlank()) return false;
-        String clean = input.replaceAll("\\s+", "").toLowerCase();
-        int left = 0;
-        int right = clean.length() - 1;
-        while (left < right) {
-            if (clean.charAt(left) != clean.charAt(right)) {
-                return false;
-            }
-            left++;
-            right--;
-        }
-        return true;
-    }
-}
+import java.util.*;
+interface PalindromeStrategy { boolean isValid(String s); }
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        PalindromeChecker service = new PalindromeChecker();
-        String phrase = "Taco Cat";
-        String sentence = "A man a plan a canal Panama";
-        System.out.println("Checking phrase: '" + phrase + "' -> " + service.checkPalindrome(phrase));
-        System.out.println("Checking sentence: '" + sentence + "' -> " + service.checkPalindrome(sentence));
+        String input = "Race Car".replaceAll("\\s+", "").toLowerCase();
+        PalindromeStrategy stackStr = s -> {
+            Stack<Character> stack = new Stack<>();
+            for (char c : s.toCharArray()) stack.push(c);
+            for (char c : s.toCharArray()) if (c != stack.pop()) return false;
+            return true;
+        };
+        PalindromeStrategy dequeStr = s -> {
+            Deque<Character> deque = new LinkedList<>();
+            for (char c : s.toCharArray()) deque.add(c);
+            while (deque.size() > 1) if (deque.pollFirst() != deque.pollLast()) return false;
+            return true;
+        };
+        System.out.println("Stack Result: " + stackStr.isValid(input));
+        System.out.println("Deque Result: " + dequeStr.isValid(input));
     }
 }
